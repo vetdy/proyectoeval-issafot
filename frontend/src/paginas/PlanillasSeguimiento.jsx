e7e18e7b5bca4ceda30559e05dfc33b04fa99e21
Ariel G. Valencia <ariel.valencia.0945@gmail.com>
2024-11-19 20:32:22 -0400

    Terminar planillas seguimiento estudiante

--- frontend/src/paginas/PlanillasSeguimiento.jsx
+++ frontend/src/paginas/PlanillasSeguimiento.jsx
@@ -6,7 +6,7 @@ import {
     obtenerPlanillasSeguimientoEmpresa,
     obtenerPlanillasEvaluacionEmpresa,
     obtenerItemsPlanillaSeguimiento,
-    obtenerItemsPlanillaEvaluacion
+    obtenerItemsPlanillaEvaluacion,
 } from "../servicios/api";
 import { tiempo } from "../utils";
 
@@ -14,6 +14,10 @@ const Planillas = ({ datos, tipo }) => {
     const titulos = ["ID", "Fecha", "Hora", "Concluido", "Detalle"];
     const [mostrar, setMostrar] = useState(datos.map((d) => false));
     const [items, setItems] = useState(datos.map((d) => null));
+
+    if (tipo === "evaluacion") {
+        titulos.push("Nota");
+    }
 
     const endpoints = {
         seguimiento: obtenerItemsPlanillaSeguimiento,
@@ -30,17 +34,19 @@ const Planillas = ({ datos, tipo }) => {
     const actualizarItem = async (fila) => {
         if (items[fila] === null) {
             const nuevoItems = [...items];
-            console.log(datos[fila]);
             const consultaItems = await endpoints[tipo](datos[fila].id);
             if (consultaItems.status === 200) {
-                if(tipo === "seguimiento"){
-                    nuevoItems[fila] = consultaItems.message.item_planilla;
+                if (tipo === "seguimiento") {
+                    nuevoItems[fila] =
+                        consultaItems.message.item_planilla.filter(
+                            (i) => i.creada === "0"
+                        );
                 }
-                if( tipo === "evaluacion" ){
+                if (tipo === "evaluacion") {
                     nuevoItems[fila] = consultaItems.message.tarea;
                 }
             }
-            if ( consultaItems.status === 404 ){
+            if (consultaItems.status === 404) {
                 nuevoItems[fila] = [];
             }
             setItems(nuevoItems);
@@ -51,52 +57,120 @@ const Planillas = ({ datos, tipo }) => {
         <Tabla key={`tabla-planilla-${tipo}`} datos={titulos}>
             {datos.map((entrada, index) => {
                 return (
-                    <Fragment key={`fila-compuesta-${index}`}>
-                        <tr key={`fila-compuesta-${index}-1`}>
+                    <Fragment key={`fila-compuesta-${tipo}-${index}`}>
+                        <tr key={`fila-compuesta-${tipo}-${index}-1`}>
                             <td>{entrada.titulo}</td>
 
-                            <td>{tiempo.normalizarFecha(entrada.fecha_revision)}</td>
+                            <td>
+                                {tiempo.normalizarFecha(entrada.fecha_revision)}
+                            </td>
 
-                            <td>{tiempo.normalizarHora(entrada.hora_revision)}</td>
+                            <td>
+                                {tiempo.normalizarHora(entrada.hora_revision)}
+                            </td>
 
                             <td>{entrada.concluido ? "Si" : "No"}</td>
 
                             <td>
-                                <button
-                                    key={`mostrar-detalle-${tipo}-fila-${index}`}
-                                    className="btn btn-eva-info"
-                                    onClick={() => mostrarDetalle(index)}
-                                >
-                                    Detalles
-                                </button>
+                                {entrada.concluido ? (
+                                    <button
+                                        key={`mostrar-detalle-${tipo}-fila-${index}`}
+                                        className="btn btn-eva-info"
+                                        onClick={() => mostrarDetalle(index)}
+                                    >
+                                        Detalle
+                                    </button>
+                                ) : (
+                                    "No disponible"
+                                )}
                             </td>
+                            {tipo === "evaluacion" && (
+                                <td>
+                                    {entrada.concluido ? entrada.nota : "-"}
+                                </td>
+                            )}
                         </tr>
 
                         <tr
-                            key={`fila-compuesta-${index}-2`}
+                            key={`fila-compuesta-${tipo}-${index}-2`}
                             className={`${mostrar[index] ? "" : "d-none"}`}
                         >
-                            <td colSpan={"5"} style={{ boxShadow: "none" }}>
-                                <div
-                                    className="py-2 border-3 rounded-2"
-                                    style={{
-                                        backgroundColor: "lightgray",
-                                    }}
-                                >
-                                    {items[index] && items[index].length ? (
-                                        <ol className="m-0">
-                                            {items[index].map((i, idx) => {
-                                                return <li>{i.titulo}</li>;
-                                            })}
-                                        </ol>
-                                    ) : (
-                                        <p>
-                                            {!items[index]
-                                                ? "Cargando..."
-                                                : "No hay datos."}
-                                        </p>
-                                    )}
-                                </div>
+                            <td
+                                colSpan={tipo === "seguimiento" ? "5" : "6"}
+                                style={{ boxShadow: "none" }}
+                                className="bg-eva-info"
+                            >
+                                {items[index] && items[index].length ? (
+                                    <div className="container-fluid">
+                                        <div className="row">
+                                            <div className="col-4 fw-bold">
+                                                Tareas
+                                            </div>
+                                            <div
+                                                className={`${
+                                                    tipo === "seguimiento"
+                                                        ? "col-8"
+                                                        : "col-7"
+                                                } fw-bold`}
+                                            >
+                                                Observaciones
+                                            </div>
+                                            {tipo === "evaluacion" && (
+                                                <div className="col-1 fw-bold">
+                                                    Nota
+                                                </div>
+                                            )}
+                                        </div>
+                                        <div className="row">
+                                            <ul className="m-0 list-group">
+                                                {items[index].map((i, idx) => {
+                                                    return (
+                                                        <li
+                                                            className="list-item"
+                                                            key={`${tipo}-fila-${index}-item-${idx}`}
+                                                        >
+                                                            <div className="container-fluid">
+                                                                <div className="row">
+                                                                    <div className="col-4">
+                                                                        {
+                                                                            i.titulo
+                                                                        }
+                                                                    </div>
+                                                                    <div
+                                                                        className={`${
+                                                                            tipo ===
+                                                                            "seguimiento"
+                                                                                ? "col-8"
+                                                                                : "col-7"
+                                                                        } `}
+                                                                    >
+                                                                        {
+                                                                            i.observacion
+                                                                        }
+                                                                    </div>
+                                                                    {tipo ===
+                                                                        "evaluacion" && (
+                                                                        <div className="col-1">
+                                                                            {
+                                                                                i.nota
+                                                                            }
+                                                                        </div>
+                                                                    )}
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                    );
+                                                })}
+                                            </ul>
+                                        </div>
+                                    </div>
+                                ) : (
+                                    <p>
+                                        {!items[index]
+                                            ? "Cargando..."
+                                            : "No hay datos."}
+                                    </p>
+                                )}
                             </td>
                         </tr>
                     </Fragment>
@@ -130,23 +204,23 @@ const PlanillasSeguimiento = () => {
         const planilla = async () => {
             const consultas = [
                 obtenerPlanillasSeguimientoEmpresa(empresa),
-                obtenerPlanillasEvaluacionEmpresa(empresa)
+                obtenerPlanillasEvaluacionEmpresa(empresa),
             ];
 
             const res = await Promise.all(consultas);
 
-            if( res.filter( r => r.status === 200 ).length === 2 ){
-                setDatosSeguimiento( res[0].message.planilla_seguimiento );
-                setDatosEvaluacion( res[1].message.evaluacion_empresa[0] );
+            if (res.filter((r) => r.status === 200).length === 2) {
+                setDatosSeguimiento(res[0].message.planilla_seguimiento);
+                setDatosEvaluacion(res[1].message.evaluacion_empresa[0]);
             }
-            /* console.log(res[0].message.planilla_seguimiento);
-            console.log(res[1].message.evaluacion_empresa[0]); */
 
-            if ( res.filter(r => r.status === 404).length ) {
+            if (res.filter((r) => r.status === 404).length) {
                 setHayPlanillas(false);
             }
 
-            if ( res.filter(r => r.status !== 200 && r.status !== 404).length ) {
+            if (
+                res.filter((r) => r.status !== 200 && r.status !== 404).length
+            ) {
                 setError(true);
             }
             setCargando(false);
@@ -185,17 +259,17 @@ const PlanillasSeguimiento = () => {
                 {hayPlanillas && (
                     <div className="col-12 d-flex justify-content-between">
                         <div>
-                            <h5>
+                            <h5 className="fw-bold">
                                 Proyecto:{" "}
-                                <span className="fw-bold">
-                                    Creación de Aulas
+                                <span className="fw-normal">
+                                    Proyecto de creación de ambientes
                                 </span>
                             </h5>
                         </div>
                         <div>
-                            <h5>
+                            <h5 className="fw-bold">
                                 Consultor:{" "}
-                                <span className="fw-bold">Marco</span>
+                                <span className="fw-normal">Mateo Corina</span>
                             </h5>
                         </div>
                     </div>
@@ -212,7 +286,10 @@ const PlanillasSeguimiento = () => {
             <div className="row">
                 <div className="col-12">
                     {hayPlanillas && (
-                        <Planillas datos={datosSeguimiento} tipo={"seguimiento"} />
+                        <Planillas
+                            datos={datosSeguimiento}
+                            tipo={"seguimiento"}
+                        />
                     )}
                 </div>
             </div>
@@ -227,14 +304,17 @@ const PlanillasSeguimiento = () => {
             <div className="row">
                 <div className="col-12">
                     {hayPlanillas && (
-                        <Planillas datos={datosEvaluacion} tipo={"evaluacion"} />
+                        <Planillas
+                            datos={datosEvaluacion}
+                            tipo={"evaluacion"}
+                        />
                     )}
                 </div>
             </div>
 
             <div className="row mt-4">
                 <div className="col g-0">
-                    <label htmlFor="idemp" className="px-2">
+                    <label htmlFor="idemp" className="py-2">
                         Empresa
                     </label>
                     <select
