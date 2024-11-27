e40425046b6c92084df1b12bdc57d32da4038621
Ariel G. Valencia <ariel.valencia.0945@gmail.com>
2024-11-26 22:18:29 -0400

    actualizar generar planillas

--- frontend/src/paginas/generarPLanillasDeSeguimientoYEvaSemanal.jsx
+++ frontend/src/paginas/generarPLanillasDeSeguimientoYEvaSemanal.jsx
@@ -1,8 +1,37 @@
 import { useState, useEffect } from "react";
+import { Modal } from "../componentes/modales";
+import { generarPlanillasProyectoEmpresa } from "../servicios/api";
+import { IconoCargando } from "../componentes/iconos";
 
 function GenerarPlanillas() {
     const [empresas, setEmpresas] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [generando, setGenerando] = useState(false);
+    const [modal, setModal] = useState({
+        mostrar: false,
+        texto: "",
+        aceptar: null,
+    })
+
+    const abrirModal = (texto, aceptar = null) => {
+        const nuevoModal = {
+            mostrar: true,
+            texto: texto,
+            aceptar: cerrarModal,
+        }
+        if( aceptar !== null ){
+            nuevoModal.aceptar = aceptar;
+        }
+        setModal(nuevoModal);
+    }
+
+    const cerrarModal = () =>{
+        setModal({
+            mostrar: false,
+            texto:"",
+            aceptar:null
+        })
+    }
 
     // Obtener empresas desde el endpoint específico
     useEffect(() => {
@@ -16,7 +45,7 @@ function GenerarPlanillas() {
                 }
     
                 const data = await response.json();
-    
+                console.log(data);
                 // Asegúrate de que los datos estén en la forma correcta
                 if (data.contenido && data.contenido.proyecto_por_docente) {
                     setEmpresas(data.contenido.proyecto_por_docente);
@@ -37,21 +66,61 @@ function GenerarPlanillas() {
     
 
     // Función para generar planilla individual
-    const generarPlanilla = (empresa) => {
+    const generarPlanilla = async (empresa) => {
         if (empresa.estado === "aceptado") {
-            alert(`Generando planilla para: ${empresa.nombre_empresa}`);
-        } else {
-            alert(`No se puede generar planilla para: ${empresa.nombre_empresa} debido a su estado (${empresa.estado})`);
+            if(empresa.planillas_creada){
+                abrirModal(`La planilla para ${empresa.nombre_empresa} ya esta generada.`);
+                return;
+            }
+            setGenerando(true);
+            const consulta = await generarPlanillasProyectoEmpresa(empresa.id_proyecto_empresa);
+            if( consulta.status === 200 ){
+                abrirModal(`Se han generado las planillas para ${empresa.nombre_empresa}`,
+                    () => {window.location.reload()}
+                );
+            }
+            else{
+                abrirModal(consulta.message);
+            }
+            setGenerando(false);
         }
     };
 
     // Función para generar planillas para todas las empresas aceptadas
-    const generarTodas = () => {
-        const aceptadas = empresas.filter((empresa) => empresa.estado === "aceptado");
-        if (aceptadas.length > 0) {
-            alert(`Generando planillas para: ${aceptadas.map((e) => e.nombre_empresa).join(", ")}`);
-        } else {
-            alert("No hay empresas aceptadas para generar planillas.");
+    const generarTodas = async () => {
+        const aceptadas = empresas.filter((empresa) => empresa.estado === "aceptado" && !empresa.planillas_creada);
+        if (!aceptadas.length) {
+            abrirModal("No hay empresas para generar planillas.");
+            return;
+        }
+        setGenerando(true);
+        const consultas = aceptadas.map( a => generarPlanillasProyectoEmpresa(a.id_proyecto_empresa) );
+        const res = await Promise.all( consultas );
+        if( res.every( r =>  r.status === 200) ){
+            abrirModal(`Se han generado planillas para: ${aceptadas.map( a => a.nombre_empresa).join(", ")}`,
+                () => {window.location.reload()}
+            );
+        }
+        else{
+            if( res.some( r => {r.status === 200}) ){
+                const done = res.filter(r => r.status === 200);
+                const fail = res.filter(r => r.status !== 200);
+                abrirModal(
+                    `Se genero planillas para: ${done
+                        .map((d) => d.nombre_empresa)
+                        .join(
+                            ", "
+                        )}.\n Fallo la generación de planillas para ${fail
+                        .map((f) => f.nombre_empresa)
+                        .join(", ")}`,
+                    () => {
+                        window.location.reload();
+                    }
+                );
+            }
+            else{
+                abrirModal("Ocurrio un error");
+            }
         }
     };
 
@@ -67,12 +136,22 @@ function GenerarPlanillas() {
 
     return (
         <div className="container mt-5">
+            {modal.mostrar && (
+                <Modal
+                    mostrar={modal.mostrar}
+                    texto={modal.texto}
+                    aceptar={modal.aceptar}
+                />
+            )}
+            {generando && (
+                <IconoCargando tipo="linea"/>
+            )}
             <h2 className="text-center mb-4">Generar Planillas de Seguimiento y Evaluación Semanal</h2>
             <table className="table table-striped ">
                 <thead className="table table-sm my-0 table-hover">
                     <tr>
                         <th scope="col" className="bg-eva-secondary text-eva-light">Empresa</th>
-                        <th scope="col" className="bg-eva-secondary text-eva-light">Estado</th>
+                        <th scope="col" className="bg-eva-secondary text-eva-light">Estado Planif.</th>
                         <th scope="col" className="bg-eva-secondary text-eva-light">Generar</th>
                     </tr>
                 </thead>
@@ -82,19 +161,31 @@ function GenerarPlanillas() {
                             <td>{empresa.nombre_empresa}</td> {/* Asegúrate de usar el campo correcto */}
                             <td>{empresa.estado}</td> {/* Usa el estado correctamente */}
                             <td>
-                                <button
-                                    className="btn fw-bold w-100 btn-eva-secondary"
-                                    onClick={() => generarPlanilla(empresa)}
-                                >
-                                    Generar
-                                </button>
+                                {empresa.estado !== "aceptado" && (
+                                    "No disponible"
+                                )}
+                                {empresa.estado === "aceptado" && empresa.planillas_creada && (
+                                    "Ya esta generada"
+                                )}
+                                {empresa.estado === "aceptado" && !empresa.planillas_creada && (
+                                    <button
+                                        className="btn fw-bold w-100 btn-eva-secondary"
+                                        onClick={() => generarPlanilla(empresa)}
+                                        disabled={generando}
+                                    >
+                                        Generar
+                                    </button>
+                                )}
                             </td>
                         </tr>
                     ))}
                 </tbody>
             </table>
             <div className="text-center">
-                <button className="btn fw-bold btn-eva-secondary" onClick={generarTodas}>
+                <button className="btn fw-bold btn-eva-secondary" 
+                    onClick={generarTodas}
+                    disabled={generando}
+                >
                     Generar todas
                 </button>
             </div>
